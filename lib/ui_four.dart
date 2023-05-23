3f4d3204b21dd22212b8c09313698ce7ec2f68fb
Samyak jain <43928825+roboanonymous@users.noreply.github.com>
2023-05-23 07:20:54 +0530

    Update ui_four.dart

--- lib/ui_four.dart
+++ lib/ui_four.dart
@@ -7,114 +7,142 @@ class UiFour extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(35),
+      padding: const EdgeInsets.all(35),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          SizedBox(height: 50,),
-          Text("Find Interested", style: TextStyle(fontWeight: FontWeight.w600, fontSize: 25),),
-          Text("Event to Join", style: TextStyle(fontWeight: FontWeight.w600, fontSize: 25),),
-          SizedBox(height: 10,),
-          Text("We share all events like charity,",style: TextStyle(color: Color.fromARGB(255, 177, 175, 175), fontSize: 15), ),
-          Text("workshop, blood drive, etc",style: TextStyle(color: Color.fromARGB(255, 177, 175, 175), fontSize: 15), ),
-          SizedBox(height: 50,),
+          const SizedBox(
+            height: 50,
+          ),
+          const Text(
+            "Find Interested",
+            style: TextStyle(fontWeight: FontWeight.w600, fontSize: 25),
+          ),
+          const Text(
+            "Event to Join",
+            style: TextStyle(fontWeight: FontWeight.w600, fontSize: 25),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const Text(
+            "We share all events like charity,",
+            style: TextStyle(
+                color: Color.fromARGB(255, 177, 175, 175), fontSize: 15),
+          ),
+          const Text(
+            "workshop, blood drive, etc",
+            style: TextStyle(
+                color: Color.fromARGB(255, 177, 175, 175), fontSize: 15),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
           Row(
             children: [
               Container(
                 height: 100,
                 width: 100,
-                decoration:   BoxDecoration(
-                image: const DecorationImage(
-                 image: AssetImage("assets/images/img5.jpg"),
-                 fit: BoxFit.fill,
+                decoration: BoxDecoration(
+                  image: const DecorationImage(
+                    image: AssetImage("assets/images/img5.jpg"),
+                    fit: BoxFit.fill,
+                  ),
+                  borderRadius: BorderRadius.circular(10),
                 ),
-                borderRadius: BorderRadius.circular(10),
               ),
+              const SizedBox(
+                width: 120,
               ),
-              SizedBox(width: 120,),
-
               Container(
                 height: 50,
                 width: 50,
-                decoration:   BoxDecoration(
-                image: const DecorationImage(
-                 image: AssetImage("assets/images/img2.jpg"),
-                 fit: BoxFit.fill,
+                decoration: BoxDecoration(
+                  image: const DecorationImage(
+                    image: AssetImage("assets/images/img2.jpg"),
+                    fit: BoxFit.fill,
+                  ),
+                  borderRadius: BorderRadius.circular(10),
                 ),
-                borderRadius: BorderRadius.circular(10),
-              ),
               ),
             ],
           ),
-
-          SizedBox(height: 50,),
-          
+          const SizedBox(
+            height: 50,
+          ),
           Center(
             child: Container(
-                  height: 120,
-                  width: 120,
-                  decoration:   BoxDecoration(
+              height: 120,
+              width: 120,
+              decoration: BoxDecoration(
                 image: const DecorationImage(
-                 image: AssetImage("assets/images/img3.jpg"),
-                 fit: BoxFit.fill,
+                  image: AssetImage("assets/images/img3.jpg"),
+                  fit: BoxFit.fill,
                 ),
-                borderRadius: BorderRadius.circular(10),
+                borderRadius: BorderRadius.circular(12),
               ),
-                ),
+            ),
           ),
-
-
-           SizedBox(height: 50,),
+          const SizedBox(
+            height: 50,
+          ),
           Row(
-            
             children: [
-               Container(
+              Container(
                 height: 60,
                 width: 60,
-                 decoration:   BoxDecoration(
-                image: const DecorationImage(
-                 image: AssetImage("assets/images/img4.jpg"),
-                 fit: BoxFit.fill,
+                decoration: BoxDecoration(
+                  image: const DecorationImage(
+                    image: AssetImage("assets/images/img4.jpg"),
+                    fit: BoxFit.fill,
+                  ),
+                  borderRadius: BorderRadius.circular(10),
                 ),
-                borderRadius: BorderRadius.circular(10),
               ),
+              const SizedBox(
+                width: 150,
               ),
-              SizedBox(width: 150,),
-
               Container(
-                
                 height: 50,
                 width: 50,
-                decoration:   BoxDecoration(
-                image: const DecorationImage(
-                 image: AssetImage("assets/images/img5.jpg"),
-                 fit: BoxFit.fill,
+                decoration: BoxDecoration(
+                  image: const DecorationImage(
+                    image: AssetImage("assets/images/img5.jpg"),
+                    fit: BoxFit.fill,
+                  ),
+                  borderRadius: BorderRadius.circular(10),
                 ),
-                borderRadius: BorderRadius.circular(10),
-              ),
               ),
             ],
           ),
-          SizedBox(height: 30,),
+          const SizedBox(
+            height: 30,
+          ),
           SizedBox(
             width: 300,
             height: 60,
-            child: ElevatedButton(onPressed: (){},
-             child: Text("Register", style: TextStyle(color: Colors.white),),
-             style: ElevatedButton.styleFrom(
-              shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20),
-             ),
-             backgroundColor: Colors.black
-
-             )
-             ),
+            child: ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  "Register",
+                  style: TextStyle(color: Colors.white),
+                ),
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    backgroundColor: Colors.black)),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text("already have an account?"),
-              TextButton(onPressed: (){}, child: Text("Sign in", style: TextStyle(color: Color.fromARGB(255, 249, 180, 62)),))
+              const Text("already have an account?"),
+              TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Sign in",
+                    style: TextStyle(color: Color.fromARGB(255, 249, 180, 62)),
+                  ))
             ],
           )
         ],
